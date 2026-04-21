bc30a6e2f3977007d202e329e4fcb2051df34248
Soham Mali <malishoham0007@gmail.com>
2026-04-21 23:03:35 +0530

    fix cors and entrypoint

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ import helmet from "helmet";
 
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 min
-    max: 100,
-    message: "Too many requests, try again later",
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 100,
+  message: "Too many requests, try again later",
 });
 
 const app = express();
@@ -20,12 +20,17 @@ app.use(helmet());
 app.use(limiter);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://job-protal-frontend-my-jobs.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 
-app.use("/api/jobs",jobRoutes);
+app.use("/api/jobs", jobRoutes);
 app.use("/api/users", userRoutes);
 app.use(errorMiddleware);
 export default app;
