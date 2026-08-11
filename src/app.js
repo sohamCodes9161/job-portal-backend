ee74485b503aca2fd6d0d6a7330bce737f23c8e4
Soham Mali <malishoham0007@gmail.com>
2026-08-11 20:14:55 +0530

    update CORS origin for frontend deployment

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
      origin: [
       "http://localhost:5173",
-      "https://job-portal-frontend-my-jobs-cm5mzb2rf-sohamcodes9161s-projects.vercel.app"
+      "https://job-portal-frontend-iota-amber.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
